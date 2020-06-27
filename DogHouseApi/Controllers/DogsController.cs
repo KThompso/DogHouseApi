fd6526104bf068a542d4079e8d44db40440e7966
Kyle Thompson-Bass <kylethompsonbass@gmail.com>
2020-06-27 09:45:04 -0400

    Implement simple dog API

---
+++ DogHouseApi/Controllers/DogsController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using DogHouseApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DogHouseApi.Controllers
+{
+
+    [Route("[controller]")]
+    [ApiController]
+    public class DogsController : ControllerBase
+    {
+
+        private static int dogCount = 0;
+        private static IDictionary<int, DogDto> dogs = new Dictionary<int, DogDto>();
+
+        // POST /dogs
+        [HttpPost]
+        public ActionResult Post([FromBody] DogDto dogDto)
+        {
+            dogs.Add(++dogCount, dogDto);
+            return CreatedAtRoute(new { id = dogCount }, dogDto);
+        }
+
+        // GET /dogs/1
+        [HttpGet]
+        public ActionResult Get() => Ok(dogs.Values);
+
+        // GET /dogs
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            if (dogs.ContainsKey(id))
+            {
+                return Ok(dogs[id]);
+            }
+
+            return NotFound();
+        }
+
+    }
+}
---
+++ DogHouseApi/Models/DogDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DogHouseApi.Models
+{
+    public class DogDto
+    {
+
+        public string Breed { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Picture { get; set; }
+
+    }
+}
